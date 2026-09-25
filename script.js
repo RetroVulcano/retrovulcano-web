5d25bb18788011d578e9db1e38f9ba0540472435
RetroVulcano <75785069+RetroVulcano@users.noreply.github.com>
2026-09-25 18:17:30 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('year').textContent=new Date().getFullYear();
+const b=document.querySelector('.hamb'),l=document.querySelector('.links');
+b.addEventListener('click',()=>{const open=l.classList.toggle('open');l.style.display=open?'flex':'';if(open){Object.assign(l.style,{position:'absolute',top:'74px',left:'0',right:'0',padding:'20px 24px',background:'#070809',flexDirection:'column',borderBottom:'1px solid #292d30'})}});
